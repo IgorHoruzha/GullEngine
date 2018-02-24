0e13f525b65595ed0971a9df0472dfb3c1457dbd
Shadowmafia <33455963+Shadowmafia@users.noreply.github.com>
2018-02-24 05:42:35 +0200

    v0.2
    Пак функций для сборки элементов сайта и сборки самог осайта.

--- index.php
+++ index.php
@@ -1,63 +1,115 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+<?php
+   
+   //возвращает хедер !
+	function siteHeader(){
+	   return '
+			<!DOCTYPE html>
+				<html lang="en">
+				<head>
 
-	  <meta charset="UTF-8">
-	  <title>gullPreviev</title>
+					  <meta charset="UTF-8">
+					  <title>gullPreviev</title>
 
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+					  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	  <link href="css/bootstrap.min.css" rel="stylesheet">
-	  <link href="css/style.css" rel="stylesheet">
+					  <link href="css/bootstrap.min.css" rel="stylesheet">
+					  <link href="css/style.css" rel="stylesheet">
 
-	  <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
-	  <script src="js/bootstrap.min.js"></script>
+					  <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+					  <script src="js/bootstrap.min.js"></script>
 
-</head>
-<body>
-	<img src="image/logo.png" id="logo"/>
-	<div  id="siteBody" class="container" >
+				</head>';
+	}
 
-		<nav class="navbar navbar-expand-lg navbar-light ">
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#Menu" aria-controls="Menu" aria-expanded="false" aria-label="Toggle navigation">
-		    <span class="navbar-toggler-icon"></span>
-		  </button>
+	//возврашает логотип сайта
+    function setLogo($logoSrc){
+    	return '<img src="'.$logoSrc.'" id="logo"/>';
+    }
+
+   // принимает асициативный масив с именами пунктов меню и их ссылками.
+   //возвращает собранное нав-бар меню.
+   function AssemblyMenu($obj,$inactiveMenuIndex){
+        $rt='<nav class="navbar navbar-expand-lg navbar-light ">
+		  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#Menu" aria-controls="Menu" aria-expanded="false" aria-label="Toggle navigation">
+		  		  <span class="navbar-toggler-icon"></span>
+		 	    </button>
 		
+		 	    <div class="collapse navbar-collapse" id="Menu">
+		   		 <ul class="navbar-nav mr-auto mt-2 mt-lg-0" id="menuContainer">';
+        $counter = 0;
+		foreach ($obj as $value) {
+            if($counter==$inactiveMenuIndex) {
+                $rt .= '<li class="nav-item active inactive">
+			        <a class="nav-link" href="#"><span class="inactive">' . $value["name"] . '</span></a>
+			      </li>';
+            }
+            else{
+                $rt .= '<li class="nav-item active">
+			        <a class="nav-link" href="' . $value["link"] . '"><span>' . $value["name"] . '</span></a>
+			      </li>';
+            }
+            $counter++;
+		}
+		$rt.='   </ul>
+			  </div>
 
-		  <div class="collapse navbar-collapse" id="Menu">
-		    <ul class="navbar-nav mr-auto mt-2 mt-lg-0" id="menuContainer">
-		      <li class="nav-item active inactive">
-		        <a class="nav-link" href="#"><span class="inactive">Home</span></a>
-		      </li>
-		      <li class="nav-item active">
-		        <a class="nav-link " href="#">Link</a>
-		      </li>
-		      <li class="nav-item active">
-		        <a class="nav-link disabled" href="#">Disabled</a>
-		      </li>
-		    </ul>
-		  </div>
+			</nav>';
 
-		</nav>
-
-		<div class="row" id=siteContent>
-			
-			
-		</div>
-
-		<div class="container row" id="footer">                   
-             <div id="copyrite" class="container container col-md-3">Copyright © 2017</div>
+		return $rt;
+   }
+   
+   //возврашает контент
+   function AssemblyContent(){
+   	 return '<div class="row" id=siteContent></div>';
+   }
+   //принимает текст для копирайтинга и масив картинов с сылками
+    //возврашает футер
+   function Assemblyfooter($imgObject,$copyright){
+   	 $rt='<div class="container row" id="footer"> 
+         <div id="copyrite" class="container container col-md-3">'.$copyright.'</div>
              <div id="social" class="container container col-md-8">
-             	<div class="container">
-	             	<div class="socialButton"><a href="https://vk.com"><img class="socialImg" src="image/socialImages/vk.png"></a></div>
-	                <div class="socialButton"><a href="https://www.facebook.com"><img class="socialImg" src="image/socialImages/facebook.png"></a></div>
-	                <div class="socialButton"><a href="https://twitter.com"><img class="socialImg" src="image/socialImages/twiter.png"></a></div>
-                </div>
+             	<div class="container">';
+           foreach ($imgObject as  $value) {
+              $rt.='<div class="socialButton"><a href="'.$value['link'].'"><img class="socialImg" src="'.$value['image'].'"></a></div>';
+           }
+         $rt.='</div>
              </div>
-		</div>       
-	</div>
+		</div>';
+		return $rt;
+   }
 
-	
+ //собирает сайт
+   function FullAsembly($logo,$menuArray,$cpy,$footerArray){
+   	 echo siteHeader();
+   	 echo '<body>';
 
-</body>
-</html>
+   	 echo setLogo($logo);
+
+   	 echo'<div  id="siteBody" class="container" >';
+   	 echo AssemblyMenu($menuArray,0);
+   	 echo AssemblyContent();
+   	 echo Assemblyfooter($footerArray, $cpy);
+
+   	 echo '	</div>
+	</body>
+	</html>';
+   }
+
+    //лого
+    $logo ='image/logo.png';
+    //пункты менюшки
+    $menuArray[0] = array("name"=>"Home","link"=>"https://www.google.com.ua");
+    $menuArray[1] = array("name"=>"Page 1","link"=>"https://www.google.com.ua");
+    $menuArray[2] = array("name"=>"Page 2","link"=>"https://www.google.com.ua");
+
+    // картинки с соцсетями в футере
+    $footArray[0] = array("link"=>"https://vk.com","image"=>"image/socialImages/vk.png");
+    $footArray[1] = array("link"=>"https://www.facebook.com","image"=>"image/socialImages/facebook.png");
+    $footArray[2] = array("link"=>"https://twitter.com","image"=>"image/socialImages/twiter.png");
+    //текст для копирайтинга
+    $cpy = "Copyright © 2017";
+
+   //поехали!
+   FullAsembly($logo, $menuArray,$cpy, $footArray);
+   
+?>
---
+++ verstka.html
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	  <meta charset="UTF-8">
+	  <title>gullPreviev</title>
+
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	  <link href="css/bootstrap.min.css" rel="stylesheet">
+	  <link href="css/style.css" rel="stylesheet">
+
+	  <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+	  <script src="js/bootstrap.min.js"></script>
+
+</head>
+<body>
+	<img src="image/logo.png" id="logo"/>
+	<div  id="siteBody" class="container" >
+
+		<nav class="navbar navbar-expand-lg navbar-light ">
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#Menu" aria-controls="Menu" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		
+
+		  <div class="collapse navbar-collapse" id="Menu">
+		    <ul class="navbar-nav mr-auto mt-2 mt-lg-0" id="menuContainer">
+		      <li class="nav-item active inactive">
+		        <a class="nav-link" href="#"><span class="inactive">Home</span></a>
+		      </li>
+		      <li class="nav-item active">
+		        <a class="nav-link " href="#">Link</a>
+		      </li>
+		      <li class="nav-item active">
+		        <a class="nav-link disabled" href="#">Disabled</a>
+		      </li>
+		    </ul>
+		  </div>
+
+		</nav>
+
+		<div class="row" id=siteContent>
+			
+			
+		</div>
+
+		<div class="container row" id="footer">                   
+             <div id="copyrite" class="container container col-md-3">Copyright © 2017</div>
+             <div id="social" class="container container col-md-8">
+             	<div class="container">
+	             	<div class="socialButton"><a href="https://vk.com"><img class="socialImg" src="image/socialImages/vk.png"></a></div>
+	                <div class="socialButton"><a href="https://www.facebook.com"><img class="socialImg" src="image/socialImages/facebook.png"></a></div>
+	                <div class="socialButton"><a href="https://twitter.com"><img class="socialImg" src="image/socialImages/twiter.png"></a></div>
+                </div>
+             </div>
+		</div>       
+	</div>
+
+	
+
+</body>
+</html>
